322526a35851734e2af8b292a00b0a98661f3ff3
quochuyslay <quochuynguyen29@gmail.com>
2021-05-22 10:55:16 +0700

    Giao diện Cuối kỳ ALTP
    Huy up lên mọi ng bổ sung và góp ý những giao diện cần thiết cho bài cuối kì

---
+++ CuoiKy/app/src/main/java/com/example/ailatrieuphu/AboutDialog.java
@@ -0,0 +1,14 @@
+package com.example.ailatrieuphu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_dialog);
+    }
+}
