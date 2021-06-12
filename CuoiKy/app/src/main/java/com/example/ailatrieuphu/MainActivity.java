e207d79ebf495c1376fa9bdaf9431bcb6ba215d7
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2021-06-12 14:50:25 +0700

    hoàn thành chức năng và fix database
    - hoàn thành chức năng gửi mail , tạo tài khoản
    - sửa lại database table User cho hợp lý

--- CuoiKy/app/src/main/java/com/example/ailatrieuphu/MainActivity.java
+++ CuoiKy/app/src/main/java/com/example/ailatrieuphu/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ailatrieuphu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    public void onClickTaoTaiKhoan(View view) {
+        Intent intent = new Intent(this,TaoTaiKhoan.class);
+        startActivity(intent);
+    }
+
+    public void onClickDangNhap(View view) {
+        Intent intent = new Intent(this,DangNhap.class);
+        startActivity(intent);
+    }
+
+    public void onClickChoiNgay(View view) {
+        Intent intent = new Intent(this,Player.class);
+        startActivity(intent);
     }
 }
--- CuoiKy/app/src/main/java/com/example/ailatrieuphu/MainMenu.java
+++ CuoiKy/app/src/main/java/com/example/ailatrieuphu/MainMenu.java
@@ -10,5 +10,6 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        CSDLAilatrieuphu csdlAilatrieuphu = new CSDLAilatrieuphu(this);
     }
 }
