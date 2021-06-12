670de634f9d88458d9e44add932284625c83783d
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2021-06-12 22:49:35 +0700

    thêm chức năng lấy danh sách

--- CuoiKy/app/src/main/java/DAO/BangXepHangDAO.java
+++ CuoiKy/app/src/main/java/DAO/BangXepHangDAO.java
@@ -1,8 +1,11 @@
 package DAO;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 import CSDL_bean.BangXepHang;
 import CSDL_bean.User;
@@ -16,5 +19,51 @@ public class BangXepHangDAO {
         long newRowId = db.insert(BangXepHang.tenBang, null, values);
         if (newRowId == -1) return -1;
         return 0;
+    }
+
+    /**
+     * @param dbHelper : CSDLAiLaTrieuPhu database
+     * @param n : -1 nếu muốn lấy hết, n >0 sẽ trả về danh sách với n kết quả
+     * @return danh sách bảng xếp hạng
+     */
+    public static ArrayList<BangXepHang> layBangXepHang(SQLiteOpenHelper dbHelper,int n){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String[] projection = {
+                BangXepHang.cotNgay,
+                BangXepHang.cotDiem
+        };
+        Cursor cursor;
+        if(n!=-1) {
+            String limit = String.valueOf(n);
+
+            cursor = db.query(
+                    BangXepHang.tenBang,   // The table to query
+                    projection,             // The array of columns to return (pass null to get all)
+                    null,              // The columns for the WHERE clause
+                    null,          // The values for the WHERE clause
+                    null,                   // don't group the rows
+                    null,                   // don't filter by row groups
+                    null,               // The sort order
+                    limit
+            );
+        }
+        else{
+            cursor = db.query(
+                    BangXepHang.tenBang,   // The table to query
+                    projection,             // The array of columns to return (pass null to get all)
+                    null,              // The columns for the WHERE clause
+                    null,          // The values for the WHERE clause
+                    null,                   // don't group the rows
+                    null,                   // don't filter by row groups
+                    null
+                    );
+        }
+        ArrayList<BangXepHang> dsBxh = new ArrayList<>();
+        while (cursor.moveToNext()){
+            String ngay = cursor.getString(cursor.getColumnIndex(BangXepHang.cotNgay));
+            int diem = cursor.getInt(cursor.getColumnIndex(BangXepHang.cotDiem));
+            dsBxh.add(new BangXepHang(ngay,diem));
+        }
+        return dsBxh;
     }
 }
