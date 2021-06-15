af6c56a1b896de66967da678492a5b5d29e805e3
Giang <hthgiang096@gmail.com>
2021-06-15 22:00:04 +0700

    call_dialog

--- CuoiKy/app/src/main/java/com/example/ailatrieuphu/CallDialog.java
+++ CuoiKy/app/src/main/java/com/example/ailatrieuphu/CallDialog.java
@@ -1,24 +1,20 @@
 package com.example.ailatrieuphu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewStub;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
+
+import CSDL_bean.CauHoi;
+import DAO.CauHoiDAO;
 
 public class CallDialog extends AppCompatActivity {
     ImageButton btn_help_call_01, btn_help_call_02, btn_help_call_03, btn_help_call_04;
@@ -26,8 +22,8 @@ public class CallDialog extends AppCompatActivity {
     TextView tvAnswer;
     RelativeLayout callsLayout;
     Button btn_close;
-    RadioButton caseARb, caseBRb, caseCRb, caseDRb;
-    RadioGroup danhSachDapAn;
+    int idCauhoi;
+    String trueAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +35,26 @@ public class CallDialog extends AppCompatActivity {
     private void setEvent() {
         getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        CauHoi cauHoi = CauHoiDAO.timCauHoiTuID(idCauhoi, new CSDLAilatrieuphu(this));
+        String da1,da2,da3,da4,dad;
+        da1 = cauHoi.getDapAn()[0];
+        da2 = cauHoi.getDapAn()[1];
+        da3 = cauHoi.getDapAn()[2];
+        da4 = cauHoi.getDapAn()[3];
+        dad = cauHoi.getDapAnDung();
+        if(da1.equals(dad)){
+            trueAnswer = "A";
+        }
+        if(da2.equals(dad)){
+            trueAnswer = "B";
+        }
+        if(da3.equals(dad)){
+            trueAnswer = "C";
+        }
+        if(da4.equals(dad)){
+            trueAnswer = "D";
+        }
         btn_help_call_01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,7 +62,24 @@ public class CallDialog extends AppCompatActivity {
                 callsLayout.addView(answerLayout);
                 answerLayout.setVisibility(View.VISIBLE);
                 answerLayout.addView(callLayout_1, 0);
-
+                if(cauHoi.getChuyenNganh()=="Bác Sĩ"){
+                    tvAnswer.setText("Theo tôi đáp án đúng là " + trueAnswer);
+                }else{
+                    Random rand = new Random();
+                    int random = rand.nextInt(4);
+                    if(random==0){
+                        tvAnswer.setText("Theo tôi đáp án đúng là A");
+                    }
+                    if(random==1){
+                        tvAnswer.setText("Theo tôi đáp án đúng là B");
+                    }
+                    if(random==2){
+                        tvAnswer.setText("Theo tôi đáp án đúng là C");
+                    }
+                    if(random==3){
+                        tvAnswer.setText("Theo tôi đáp án đúng là D");
+                    }
+                }
             }
         });
         btn_help_call_02.setOnClickListener(new View.OnClickListener() {
@@ -79,12 +112,15 @@ public class CallDialog extends AppCompatActivity {
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CallDialog.this.finishAffinity();
+                CallDialog.this.finish();
+                //CallDialog.this.finishAffinity();
             }
         });
     }
 
     private void setControl() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         btn_help_call_01= findViewById(R.id.btn_help_01);
         btn_help_call_02= findViewById(R.id.btn_help_02);
         btn_help_call_03= findViewById(R.id.btn_help_03);
